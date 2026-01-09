66d2c611a89dbc4d8af5806b69428c72c3127726
alex <let.value@gmail.com>
2026-01-09 20:22:09 +0500

    feat: Implement ClimateTargetPointMixin and MultiNoiseBiomeSourceMixin for enhanced biome parameter access

---
+++ fabric/src/main/java/com/terrasect/fabric/mixin/MultiNoiseBiomeSourceAccessor.java
@@ -0,0 +1,23 @@
+package com.terrasect.fabric.mixin;
+
+import com.mojang.datafixers.util.Either;
+import com.terrasect.common.mixin.MultiNoiseBiomeSourceAccessor;
+import net.minecraft.core.Holder;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Climate;
+import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
+import net.minecraft.world.level.biome.MultiNoiseBiomeSourceParameterList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(MultiNoiseBiomeSource.class)
+public interface MultiNoiseBiomeSourceMixin extends MultiNoiseBiomeSourceAccessor {
+    /**
+     * Access the parameters field.
+     * Left: Direct parameter list
+     * Right: Holder to a parameter list preset (e.g., OVERWORLD)
+     */
+    @Accessor("parameters")
+    @Override
+    Either<Climate.ParameterList<Holder<Biome>>, Holder<MultiNoiseBiomeSourceParameterList>> terrasect$getParameters();
+}
