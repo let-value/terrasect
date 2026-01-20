bdd3561df57ee43b8393cc897d2ec37283b3644a
alex <let.value@gmail.com>
2026-01-20 23:15:21 +0500

    Add ExampleServerMixin for Fabric and NeoForge integration

---
+++ fabric/src/main/java/com/letvalue/terrasect/mixin/ExampleServerMixin.java
@@ -0,0 +1,31 @@
+package com.letvalue.terrasect.mixin;
+
+import com.letvalue.terrasect.TerrasectConstants;
+import net.minecraft.commands.CommandSource;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.ServerInfo;
+import net.minecraft.server.TickTask;
+import net.minecraft.util.thread.ReentrantBlockableEventLoop;
+import net.minecraft.world.level.chunk.storage.ChunkIOErrorReporter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Example server mixin for Fabric.
+ * Mixins should be written in Java, not Kotlin.
+ */
+@Mixin(MinecraftServer.class)
+public abstract class ExampleServerMixin extends ReentrantBlockableEventLoop<TickTask> 
+        implements ServerInfo, ChunkIOErrorReporter, CommandSource, AutoCloseable {
+    
+    public ExampleServerMixin(String name) {
+        super(name);
+    }
+
+    @Inject(method = "loadLevel", at = @At("TAIL"))
+    private void terrasect$onLoadLevel(CallbackInfo ci) {
+        System.out.println("[" + TerrasectConstants.MOD_NAME + "] Server level loaded! (modid: " + TerrasectConstants.MOD_ID + ")");
+    }
+}
