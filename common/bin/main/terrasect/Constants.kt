9222471026a297b85cade364ecea217dc8762ad4
alex <let.value@gmail.com>
2026-05-23 14:59:55 +0500

    chore: ignore generated bin outputs

--- common/bin/main/terrasect/Constants.kt
+++
@@ -1,6 +0,0 @@
-package terrasect
-
-object Constants {
-  const val MOD_ID = "terrasect"
-  const val MOD_NAME = "Terrasect"
-}
--- common/bin/main/terrasect/presets/Index.kt
+++
@@ -1,5 +0,0 @@
-package terrasect.presets
-
-enum class Presets(val id: String) {
-  CLIMATE_DEBUG("climate_debug")
-}
