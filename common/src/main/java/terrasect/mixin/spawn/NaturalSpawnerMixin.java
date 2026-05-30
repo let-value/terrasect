9ae5b750788d8f9f9bbbb78fe54e8dacec36a8df
alex <let.value@gmail.com>
2026-05-30 17:58:39 +0500

    feat: add mob spawn constraints runtime slice

---
+++ common/src/main/java/terrasect/mixin/spawn/NaturalSpawnerMixin.java
@@ -0,0 +1,33 @@
+package terrasect.mixin.spawn;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.NaturalSpawner;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import terrasect.handler.MobHandler;
+
+@Mixin(NaturalSpawner.class)
+public class NaturalSpawnerMixin {
+
+  @WrapOperation(
+      method =
+          "spawnCategoryForPosition(Lnet/minecraft/world/entity/MobCategory;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkAccess;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/NaturalSpawner$SpawnPredicate;Lnet/minecraft/world/level/NaturalSpawner$AfterSpawnCallback;)V",
+      at =
+          @At(
+              value = "INVOKE",
+              target =
+                  "Lnet/minecraft/world/level/NaturalSpawner$SpawnPredicate;test(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/chunk/ChunkAccess;)Z"))
+  private static boolean terrasect$filterMobSpawn(
+      NaturalSpawner.SpawnPredicate predicate,
+      EntityType<?> entityType,
+      BlockPos blockPos,
+      ChunkAccess chunkAccess,
+      Operation<Boolean> original) {
+    if (!MobHandler.allowSpawn(chunkAccess, blockPos.getX(), blockPos.getZ(), entityType)) return false;
+    return original.call(predicate, entityType, blockPos, chunkAccess);
+  }
+}
---
+++ common/src/main/kotlin/terrasect/handler/MobHandler.kt
@@ -0,0 +1,21 @@
+package terrasect.handler
+
+import net.minecraft.world.entity.EntityType
+import net.minecraft.world.level.chunk.ChunkAccess
+import terrasect.extender.ChunkAccessExtender
+import terrasect.instrumentation.TerrasectInstr
+import terrasect.instrumentation.TerrasectMetricEvent
+
+private val instr = TerrasectInstr.mob
+
+object MobHandler {
+  @JvmStatic
+  fun allowSpawn(chunkAccess: ChunkAccess, blockX: Int, blockZ: Int, entityType: EntityType<*>): Boolean {
+    val chunkContext = (chunkAccess as ChunkAccessExtender).`terrasect$getContext`() ?: return true
+    val ctx = chunkContext.dimensionContext ?: return true
+    val lookup = ctx.mobLookup ?: return true
+    val region = chunkContext.getRegion(blockX, blockZ) ?: return true
+    instr.count(TerrasectMetricEvent.MOB_APPLIED)
+    return lookup.allowSpawn(region, entityType)
+  }
+}
